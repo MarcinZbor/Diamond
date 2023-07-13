3475f7cbf2ab3e35d58b0e910032f80e3c472b63
MarcinZbor <marcinzb@gazeta.pl>
2023-07-13 13:44:06 +0200

    burger

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const navMobile = document.querySelector(".nav-mobile");
+const navBtn = document.querySelector(".hamburger");
+const footerYear = document.querySelector(".footer__year");
+
+const handleBurger = () => {
+	navBtn.classList.toggle("is-active");
+	navMobile.classList.toggle("nav-mobile--active");
+	document.body.classList.toggle("sticky-body");
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+
+navBtn.addEventListener("click", handleBurger);
