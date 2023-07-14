d3299dc976199ed0e8c7c1a819eb449f491968d9
MarcinZbor <marcinzb@gazeta.pl>
2023-07-14 10:32:18 +0200

    finishall

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
 const footerYear = document.querySelector(".footer__year");
+const allNavItems = document.querySelectorAll(".nav__link");
 
 const handleBurger = () => {
 	navBtn.classList.toggle("is-active");
 	navMobile.classList.toggle("nav-mobile--active");
 	document.body.classList.toggle("sticky-body");
+
+	allNavItems.forEach((item) =>
+		item.addEventListener("click", () => {
+			navMobile.classList.remove("nav-mobile--active");
+			document.body.classList.remove("sticky-body");
+		})
+	);
+
+	allNavItems.forEach((item) =>
+		item.addEventListener("click", () => {
+			navBtn.classList.remove("is-active");
+		})
+	);
 };
 
 const handleCurrentYear = () => {
